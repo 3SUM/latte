3ef1f3eccc08d041849480438b4c865685411610
Luis Maya Aranda <mayafluis@gmail.com>
2022-09-21 23:02:17 -0700

    Progress on engine and added sample main

--- src/engine.hpp
+++ src/engine.hpp
@@ -11,9 +11,28 @@ class Value {
     std::string op;
 
    public:
-    Value() = delete;
     Value(T _data) : data(_data), op("") {}
     Value(T _data, std::string _op) : data(_data), op(_op) {}
+    Value(const Value<T>& rhs) : data(rhs.data), op(rhs.op) {}
+
+    const Value<T>& operator=(const Value<T>& rhs) {
+        this->data = rhs.data;
+        this->op = rhs.op;
+        return *this;
+    }
+
+    Value operator+(const Value& rhs) {
+        return Value(this->data + rhs.data, "+");
+    }
+
+    Value operator*(const Value& rhs) {
+        return Value(this->data * rhs.data, "*");
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Value& v) {
+        os << "Value (data = " << v.data << ")";
+        return os;
+    }
 };
 
 }  // namespace speedy
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
 #include "engine.hpp"
 
 using namespace speedy;
 
 int main() {
-    Value<int>(10);
+    Value<int> a(10);
+    Value<int> b(15);
+    Value<int> c = a + b;
+    Value<int> d(c);
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
     return 0;
 }
